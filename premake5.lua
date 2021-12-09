e5a21200a88ee1fc586436e68d947eb05c1ce2a8
Count_MHM <71083058+Count-MHM@users.noreply.github.com>
2021-12-09 17:49:26 +0330

    Updated Premake

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,11 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-	
+    staticruntime "off"
+
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-	
+
 	files
 	{
 		"imconfig.h",
@@ -12,17 +13,27 @@ project "ImGui"
 		"imgui.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
+		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
 		"imgui_demo.cpp"
 	}
-	
+
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
-	
-	filter { "system:windows", "configurations:Release" }
-		buildoptions "/MT"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
